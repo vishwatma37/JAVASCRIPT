15370eaca4ef7a31ecb0e7287a4d091ed75b0bdf
parmatmaau <parmatma.yadavau@gmail.com>
2025-09-19 22:25:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const currentTime = () => {
+//   let curTime = new Date().toLocaleTimeString();
+//   document.getElementById("clock").innerHTML = curTime;
+// };
+// currentTime();
+
+// const intervalid = setInterval(() => {
+//   currentTime();
+// }, 1000);
+
+// setTimeout(() => clearInterval(intervalid), 5000);
