48799603b16df096c56f65e26c772c435cf3430a
parmatmaau <parmatma.yadavau@gmail.com>
2025-09-26 18:50:59 +0530

    first commit

--- script.js
+++ script.js
@@ -1,57 +1,27 @@
-// Topics in our Advanced JavaScript.
-// Event Propagation (event bubbling and event capturing)
-// Higher Order Function
-// Callback Function
-// Closures & function Currying(we will see after Async JS section)
-// Callback Hell
-// Fetch API
-// Promises
-// Async-Await
-// Error Handling in JS
+const apiBody = document.querySelector(".api_body");
 
-// Event Propagation (event bubbling and event capturing)
-// event propagation -- top to down
-// event Bubbling --down to top
-// Event Propagation - event Propagation refers  to the process of how events
-// progpagate or travel throught the DOM (Document Object Model ) hierarchy-
-//     In javaScript , there are two phases  of event propagation capturing phase
-//     and bubbling phase.
+// const apiUrl = "https://jsonplaceholder.typicode.com/todos/1"
 
-const callOuter = (event) => {
-  console.table([
-    {
-      Description: "I'm the outer one",
-      Target: event.target,
-      CurrentTarget: event.currentTarget,
+const apiUrl = "https://icanhazdadjoke.com";
+const fetchData = () => {
+  fetch(apiUrl, {
+    headers: {
+      Accept: "application/json",
     },
-  ]);
+  })
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data.joke);
+      apiBody.innerHTML = data.joke;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
-const callMiddle = (event) => {
-  console.table([
-    {
-      Description: "I'am the Middle one",
-      Target: event.target,
-      CurrentTarget: event.currentTarget,
-    },
-  ]);
-};
+fetchData();
 
-const callInner = (event) => {
-  console.table([
-    {
-      Description: "I'am the inner one",
-      Target: event.target,
-      CurrentTarget: event.currentTarget,
-    },
-  ]);
-//   event.stopPropagation();
-};
-
-// document.getElementById("outer").addEventListener("click", callOuter);
-// document.getElementById("middle").addEventListener("click", callMiddle);
-// document.getElementById("inner").addEventListener("click", callInner);
-
-document.getElementById("outer").addEventListener("click", callOuter,true);
-document.getElementById("middle").addEventListener("click", callMiddle,true);
-document.getElementById("inner").addEventListener("click", callInner,true);
+document.getElementById("fetchjoke").addEventListener("click", fetchData);
