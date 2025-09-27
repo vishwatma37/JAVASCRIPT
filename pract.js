1f9448d2093201e4870ff825ef7ff6b643b688c2
parmatmaau <parmatma.yadavau@gmail.com>
2025-09-28 00:35:07 +0530

    first commit

---
+++ pract.js
@@ -0,0 +1,18 @@
+// let dt = 1708667988;
+// const curDate = new Date(dt * 1000); //convert seconds to milliseconds
+// console.log(curDate);
+
+// const options = {
+//   weekday: "long",
+//   year: "numeric",
+//   month: "long",
+//   day: "numeric",
+//   hour: "numeric",
+//   minute: "numeric",
+// //   second: "numeric",
+// };
+// const formatter = new Intl.DateTimeFormat("en-us",options);
+
+// const formattedDate = formatter.format(curDate);
+
+// console.log(formattedDate);
--- script.js
+++ script.js
@@ -1,23 +1,82 @@
-const apiBody = document.querySelector(".api_body");
-const apiUrl = "https://icanhazdadjoke.com";
+let cityName = document.querySelector(".weather_city");
+// cityName.textContent = "Pune, India";
+let dateTime = document.querySelector(".weather_date_time");
+let w_forecast = document.querySelector(".weather_forecast");
+let w_temperature = document.querySelector(".weather_temperature");
+let w_icon = document.querySelector(".weather_icon");
+let w_minTem = document.querySelector(".weather_min");
+let w_maxTem = document.querySelector(".weather_max");
 
-// async function fetchData() {
-  const fetchData = async () => {
-  try { 
-  const res = await fetch(apiUrl, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
-  const data = await res.json();
-  console.log(data.joke);
-  apiBody.innerHTML = data.joke;
-  } catch(error){
-    apiBody.innerHTML = 'error!!!!!!';
-      console.log(error);
+let w_feelsLike = document.querySelector(".weather_feelsLike");
+let w_humidity = document.querySelector(".weather_humidity");
+let w_wind = document.querySelector(".weather_wind");
+let w_pressure = document.querySelector(".weather_pressure");
+let citySearch = document.querySelector(".weather__search");
+
+const getCountryName = (code) => {
+  return new Intl.DisplayNames([code], { type: "region" }).of(code);
+};
+
+const getDateTime = (dt) => {
+  dt = 1708667988;
+  const curDate = new Date(dt * 1000); //convert seconds to milliseconds
+  console.log(curDate);
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  const formatter = new Intl.DateTimeFormat("en-us", options);
+  console.log(formatter);
+  return formatter.format(curDate);
+};
+
+let city = "pune";
+
+citySearch.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let cityName = document.querySelector(".city_name");
+  console.log(cityName.value);
+  city = cityName.value;
+  getWeatherData();
+  cityName.value = "";
+});
+
+const getWeatherData = async () => {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7b55b5cfbe22f59878dc6c82535bb839&units=metric`;
+
+  try {
+    const res = await fetch(weatherUrl);
+    const data = await res.json();
+    console.log(data);
+
+    const { main, name, weather, wind, sys, dt } = data;
+
+    cityName.innerHTML = `${name},${getCountryName(sys.country)}`;
+    dateTime.innerHTML = getDateTime(dt);
+    w_forecast.innerHTML = weather[0].main;
+    // w_icon.innerHTML = `${weather[0].icon}`;      ////remind this
+    let iconCode = weather[0].icon;
+    let iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    w_icon.innerHTML = `<img src="${iconUrl}" alt="Weather Icon">`;
+
+    w_temperature.innerHTML = `${main.temp}&#176`;
+    w_minTem.innerHTML = `Min: ${main.temp_min.toFixed()}&#176`;
+    w_maxTem.innerHTML = `Min: ${main.temp_max.toFixed()}&#176`;
+
+    w_feelsLike.innerHTML = `${main.feels_like.toFixed(2)}&#176`;
+    w_humidity.innerHTML = `${main.humidity}%`;
+    w_wind.innerHTML = `${wind.speed}m/s`;
+    w_pressure.innerHTML = `${main.pressure}hpa`;
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
-fetchData();
-
-document.getElementById("fetchjoke").addEventListener("click", fetchData);
+document.body.addEventListener("load", getWeatherData());
+// window.addEventListener("load", getWeatherData);
